455ef3b540e186b95097b1850cf2b2f56e3da52c
shaha <shahafbenjakov@gmail.com>
2020-01-23 09:00:14 +0200

    support search filters

--- server-demo/db/api/get-apartments.js
+++ server-demo/db/api/get-apartments.js
@@ -2,16 +2,14 @@ const connection = require('../config');
 const Builder = require('./builders/apartments-builder');
 
 
-function getAll({city_id, price, number_of_room, number_of_bath, sqft, sale_status, property_type, page = 1, size = 10}) {
+function getAll({city_name, price, number_of_room, number_of_bath, property_type, page, size}) {
     return new Promise((resolve, reject) => {
         try{
             const {query,params} = Builder.allApartments(page, size)
-                            .cityId(city_id)
+                            .cityName(city_name)
                             .price(price)
                             .numberOfRoom(number_of_room)
                             .numberOfBath(number_of_bath)
-                            .sqft(sqft)
-                            .saleStatus(sale_status)
                             .propertyType(property_type)
                             .build();
             connection.query(query, params, (error, results, fields) => {
