e853c332486213aa2190004ab14ca6d022d7b0a1
shaha <shahafbenjakov@gmail.com>
2020-01-27 11:21:48 +0200

    bugs fix

--- server-demo/db/api/get-apartments.js
+++ server-demo/db/api/get-apartments.js
@@ -2,7 +2,7 @@ const connection = require('../config');
 const Builder = require('./builders/apartments-builder');
 
 
-function getAll({city_name, price, number_of_room, number_of_bath, property_type, page, size}) {
+function getAll({city_name, price, number_of_room, number_of_bath, property_type, page=1, size=9}) {
     return new Promise((resolve, reject) => {
         try{
             const {query,params} = Builder.allApartments(page, size)
